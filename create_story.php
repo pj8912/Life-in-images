d33815b137d1708b216451f1ea0a7940de32a5a8
pj8912 <59218902+pj8912@users.noreply.github.com>
2020-11-02 17:44:22 +0530

    Update create_story.php

--- create_story.php
+++ create_story.php
@@ -1,6 +1,8 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <meta charset="utf-8">
     <title>Create Story</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
