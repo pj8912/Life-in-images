0f1c033b2716e3e652b3c5f475b4200403b4c689
pj8912 <59218902+pj8912@users.noreply.github.com>
2020-11-02 17:43:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 <h1 class="mt-5" align="center">Life In Images</h1>
 
