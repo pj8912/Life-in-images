3ff628592ef7eee5e171f0e7b07f3eb2cea97bbb
pj8912 <59218902+pj8912@users.noreply.github.com>
2021-03-31 09:39:16 +0530

    Create lii.sql

---
+++ lii.sql
@@ -0,0 +1,16 @@
+create DATABASE lim;
+
+CREATE TABLE profile
+(
+    p_id int AUTO_INCREMENT PRIMARY KEY NOT NULL,  
+    pro_name text,
+    pro_pic VARCHAR(300)
+);
+
+    create table story
+    (   id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+        image VARCHAR(300),
+        caption text,
+        p_id INT,
+        FOREIGN KEY(p_id) REFERENCES profile(p_id)
+    );
